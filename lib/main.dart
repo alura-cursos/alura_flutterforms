fe349874eea3c12318d641b23695e15f037669fe
Leonardo Marinho <leonardomarinho.nf@gmail.com>
2021-01-12 14:50:20 -0300

    Vídeo 1.3

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class BytebankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Colors.green[900],
         accentColor: Color.fromRGBO(71, 161, 56, 1),
--- lib/screens/autenticacao/login.dart
+++ lib/screens/autenticacao/login.dart
@@ -4,7 +4,53 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Padding(
+        padding: EdgeInsets.only(top: 40),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topCenter,
+                child: Image.asset(
+                  'assets/images/bytebank_logo.png',
+                  width: 200,
+                ),
+              ),
+
+              SizedBox(height: 30,),
+
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  width: 300,
+                  height: 430,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(12)
+                  ),
+
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        Text(
+                            'Faça seu Login',
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 20,)
+            ],
+          ),
+        ),
+      ),
       backgroundColor: Theme.of(context).accentColor,
     );
   }
