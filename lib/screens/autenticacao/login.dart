ba662d9f52114f5ea60b62ea90509b1471c413bf
Leonardo Marinho <leonardomarinho.nf@gmail.com>
2021-01-12 14:53:32 -0300

    Vídeo 1.4

--- lib/screens/autenticacao/login.dart
+++ lib/screens/autenticacao/login.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
+
+  TextEditingController _cpfController = TextEditingController();
+  TextEditingController _senhaController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,6 +42,45 @@ class Login extends StatelessWidget {
                           style: TextStyle(
                               fontSize: 20,
                               fontWeight: FontWeight.bold,
+                          ),
+                        ),
+
+                        SizedBox(height: 15,),
+
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: 'CPF',
+                          ),
+                          maxLength: 11,
+                          keyboardType: TextInputType.number,
+                          controller: _cpfController,
+                        ),
+
+                        SizedBox(height: 20,),
+
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Senha',
+                          ),
+                          maxLength: 15,
+                          keyboardType: TextInputType.text,
+                          controller: _senhaController,
+                        ),
+
+                        SizedBox(height: 30,),
+
+                        SizedBox(
+                          width: double.infinity,
+                          child: OutlineButton(
+                            textColor: Theme.of(context).accentColor,
+                            highlightColor: Color.fromRGBO(
+                                71, 161, 56, 0.2
+                            ),
+                            borderSide: BorderSide(
+                              width: 2,
+                              color: Theme.of(context).accentColor
+                            ),
+                            child: Text('CONTINUAR'),
                           ),
                         ),
                       ],
